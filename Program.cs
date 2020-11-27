5a27d6faf328e35770d606d33b6fdde79e98f044
murcmp4 <Kurwaldq1>
2020-11-27 14:43:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SNA_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String password = "pass";
+            int trying = 1;
+            do
+            {
+                Console.Write("Введите пароль - ");
+                Console.Write("Попытка - ");
+                Console.WriteLine(trying);
+                trying++;
+                password = Convert.ToString(Console.ReadLine());
+                if (password == "pass")
+                {
+                    Console.Write("Ключ под ковриком");
+                    break;
+                }
+            } while (trying <= 3);
+        }
+    }
+}
